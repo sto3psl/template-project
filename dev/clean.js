01818e082056323a59e6fdd59878c01425e163a4
Fabian Gündel <stoepsl93@gmx.de>
2015-11-06 09:05:44 +0100

    cleanup

--- dev/clean.js
+++ dev/clean.js
@@ -1,12 +1,6 @@
 'use strict'
 
 var del = require('node-delete')
-var fs = require('fs')
-
-// fs.readdir('../dist', function (err, files) {
-//   if (err) console.log(err)
-//   else console.log(files)
-// })
 
 const dir = '../dist/'
 
